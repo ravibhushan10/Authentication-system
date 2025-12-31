cf2eb09764b7efa29a03bde1993a3a352124388f
Ravi Bhushan <ravibhushankumar87tp@gmail.com>
2026-01-01 02:24:26 +0530

    url updated

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,6 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: import.meta.env.REACT_APP_API_URL || 'http://localhost:5000' });
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  credentials: 'include' 
+});
 
 export const apiSlice = createApi({
   baseQuery,
